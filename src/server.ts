508cc6cff58b5054be3bba10f7931fce858f9ae7
Thiego Moura <thi-ego@hotmail.com>
2021-09-07 01:18:57 -0400

    configure routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import routes from './routes';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.API_PORT;
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(port || 3333, () => {
     if (process.env.NODE_ENV === 'dev')
